ab00d9bad85db842d66a5e150cde61479aab4c31
Numan <66954991+numanhussainteam@users.noreply.github.com>
2020-08-06 00:05:28 +0600

    added weather design

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,21 @@
           type="text" 
           class="search-bar" 
           placeholder="Search..."
-
        />
-
      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">bangladesh</div>
+          <div class="date">10.06.2020 manday</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">nnnnn</div>
+        </div>
+      </div>
+
 
     </main> 
   </div>
@@ -97,7 +108,48 @@ search box
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
 
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 
 
 </style>
